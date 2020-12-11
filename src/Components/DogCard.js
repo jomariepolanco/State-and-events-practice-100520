728263c88f2a7527194683f707366530048b50bb
jomariepolanco <jomariepolanco@gmail.com>
2020-12-11 09:34:44 -0500

    changed ternary to null rather than empty string

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -23,7 +23,7 @@ class DogCard extends React.Component{
         <span className="bark">
           <button onClick={this.handleClick}>Bark</button>
         </span>
-        {this.state.bark === false ? "" : <h3>Bark</h3>}
+        {this.state.bark === false ? null : <h3>Bark</h3>}
       </div>
     );
   }
