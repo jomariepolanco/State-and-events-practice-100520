ac3e571e5750ba32a3d83a0b9cfb5ebecd0d696a
jomariepolanco <jomariepolanco@gmail.com>
2020-12-11 09:49:44 -0500

    refactored click event

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -5,15 +5,12 @@ class DogCard extends React.Component{
   state = {bark: false}
 
   handleClick = () => {
-    if (this.state.bark === false){
-      this.setState((prevState) => ({bark: true}))
-    } else if (this.state.bark === true){
-      this.setState((prevState) => ({bark: false}))
-    }
+      this.setState((prevState) => ({bark: !prevState.bark}))
   }
 
 
   render(){
+    // console.log(this.state.bark)
     return (
       <div className="card">
         <span className="content">
@@ -23,7 +20,7 @@ class DogCard extends React.Component{
         <span className="bark">
           <button onClick={this.handleClick}>Bark</button>
         </span>
-        {this.state.bark === false ? null : <h3>Bark</h3>}
+        {this.state.bark ? <h3>Bark</h3>: null}
       </div>
     );
   }
